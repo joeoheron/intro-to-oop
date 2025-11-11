b43d3d717888688b1eeef48dbdb512238ca6e4b0
Joe <joeoheron@gmail.com>
2025-11-11 09:41:58 +0000

    added acsii and base user class

---
+++ assignment/user.py
@@ -0,0 +1,21 @@
+class User:
+    def __init__(self, name, email, country):
+        self.name = name
+        self.email = email
+        self.country = country
+
+
+def createUser(self):
+    return
+
+
+def depositFunds(self):
+    return
+
+
+def convertFunds(self):
+    return
+
+
+def withdrawFunds(self):
+    return
