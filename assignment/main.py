0e48db88a5db2f0ad4ef51cb8622c49be84b6a2e
Joe <joeoheron@gmail.com>
2025-11-11 12:51:48 +0000

    removed 'limited' from acsii

--- assignment/main.py
+++ assignment/main.py
@@ -4,13 +4,15 @@ from client import Client
 def main():
     # ACSII Art Generated at https://patorjk.com/software/taag
     print("""
-  ___ _   _ ___ ___   ___            _   _             _    _       _ _          _
- / __| | | | _ ) __| | _ ) __ _ _ _ | |_(_)_ _  __ _  | |  (_)_ __ (_) |_ ___ __| |
-| (__| |_| | _ \\__ \\ | _ \\/ _` | ' \\| / / | ' \\/ _` | | |__| | '  \\| |  _/ -_) _` |
- \\___|\\___/|___/___/ |___/\\__,_|_||_|_\\_\\_|_||_\\__, | |____|_|_|_|_|_|\\__\\___\\__,_|
+  ___ _   _ ___ ___   ___            _   _
+ / __| | | | _ ) __| | _ ) __ _ _ _ | |_(_)_ _  __ _
+| (__| |_| | _ \\__ \\ | _ \\/ _` | ' \\| / / | ' \\/ _` |
+ \\___|\\___/|___/___/ |___/\\__,_|_||_|_\\_\\_|_||_\\__, |
                                                |___/
         """)
-    print("Welcome to CUBS Banking's online services! What can we help you with today?")
+    print(
+        "Welcome to CUBS Banking Limited's online services! What can we help you with today?"
+    )
     print()
 
     client = Client()
