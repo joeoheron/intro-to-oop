7c4e49253f99e9708db5a4da2ec53b559f8852a6
Joe <joeoheron@gmail.com>
2025-11-14 17:06:00 +0000

    some exception handled added

--- assignment/bank_account.py
+++ assignment/bank_account.py
@@ -14,34 +14,56 @@ class Bank_Account:
         # https://www.w3schools.com/python/python_strings.asp
         self.id = f"{user.country[0:2].upper()}{format(random.randint(0, 99999999999999), '014d')}"
         self.user = user
-        type = int(
-            input(
-                "\nWhat type of account would you like to open?\n\n[1] Personal Current Account\n[2] Business Current Account\n\n"
-            )
-        )
-        if type == 1:
-            self.type = "personal"
-        elif type == 2:
-            self.type = "business"
-        else:
+
+        # Using this loop for exception handling:
+        # https://stackoverflow.com/a/2244307
+        while True:
+            try:
+                type = int(
+                    input(
+                        "\nWhat type of account would you like to open?\n\n[1] Personal Current Account\n[2] Business Current Account\n\n"
+                    )
+                )
+                if type == 1:
+                    self.type = "personal"
+                    break
+                elif type == 2:
+                    self.type = "business"
+                    break
+            except ValueError:
+                pass
+
             print(
-                "Invalid option. Please only enter a [1] for a personal account or a [2] for a business account."
+                "\nInvalid option. Please only enter either a 1 for a personal bank account or a 2 for a business bank account."
             )
 
         self.balance = 0.00
         available_currencies = {"€": "eur", "$": "usd", "£": "gbp"}
-        chosen_currency = int(
-            input(
-                f"\nWhat currency would you like this account hold?\n\n[1] {list(available_currencies.keys())[0].upper()} ({list(available_currencies.values())[0].upper()})\n[2] {list(available_currencies.keys())[1].upper()} ({list(available_currencies.values())[1].upper()})\n[3] {list(available_currencies.keys())[2].upper()} ({list(available_currencies.values())[2].upper()})\n\n"
-            )
-        )
 
-        if chosen_currency == 1:
-            self.currency = list(available_currencies.keys())[0]
-        elif chosen_currency == 2:
-            self.currency = list(available_currencies.keys())[1]
-        elif chosen_currency == 3:
-            self.currency = list(available_currencies.keys())[2]
+        while True:
+            try:
+                chosen_currency = int(
+                    input(
+                        f"\nWhat currency would you like this account hold?\n\n[1] {list(available_currencies.keys())[0].upper()} ({list(available_currencies.values())[0].upper()})\n[2] {list(available_currencies.keys())[1].upper()} ({list(available_currencies.values())[1].upper()})\n[3] {list(available_currencies.keys())[2].upper()} ({list(available_currencies.values())[2].upper()})\n\n"
+                    )
+                )
+
+                if chosen_currency == 1:
+                    self.currency = list(available_currencies.keys())[0]
+                    break
+                elif chosen_currency == 2:
+                    self.currency = list(available_currencies.keys())[1]
+                    break
+                elif chosen_currency == 3:
+                    self.currency = list(available_currencies.keys())[2]
+                    break
+
+            except ValueError:
+                pass
+
+            print(
+                "\nInvalid option. Please only enter either a 1 for EUR, a 2 for USD, or a 3 for GBP."
+            )
 
         currency_counter = 0
         for bank_account in self.user.bank_accounts:
@@ -59,39 +81,43 @@ class Bank_Account:
         self.transactions = []
 
         print("\nNew account created successfully!\n")
-        self.show_overview()
+        self.show_detailed()
 
     # Show bank account overview
     def show_overview(self, index=1):
-        print("\t _______________________________________________________")
-        print(f"\t| {index} |\t\t\t\t\t\t\t|\n\t|‾‾‾\t\t\t\t\t\t\t|")
-        print(f"\t|\tAccount Nickname:\t{self.nickname}\t\t|")
-        print(f"\t|\tAccount ID:\t\t{self.id}\t|")
-        print(f"\t|\tAccount Type:\t\t{self.type.capitalize()}\t\t|")
+        print("      _______________________________________________________")
+        print(f"     | {index} |\t\t\t\t\t\t     |\n     |‾‾‾\t\t\t\t\t\t     |")
+        print(f"     |\t    Account Nickname:\t{self.nickname}\t\t     |")
+        print(f"     |\t    Account ID:\t\t{self.id}\t     |")
+        print(f"     |\t    Account Type:\t{self.type.capitalize()}\t\t     |")
         # Printing the account balance, rounding nicely with .2f:
         # https://www.datacamp.com/tutorial/python-round-to-two-decimal-places
         if self.balance < 1000:
-            print(f"\t|\tBalance:\t\t{self.currency}{self.balance:.2f}\t\t\t|")
+            print(
+                f"     |\t    Balance:\t\t{self.currency}{self.balance:.2f}\t\t\t     |"
+            )
         elif self.balance >= 1000 and self.balance < 10000000000:
-            print(f"\t|\tBalance:\t\t{self.currency}{self.balance:.2f}\t\t|")
+            print(
+                f"     |\t    Balance:\t\t{self.currency}{self.balance:.2f}\t\t     |"
+            )
         else:
-            print(f"\t|\tBalance:\t\t{self.currency}{self.balance:.2f}\t|")
-        print("\t|\t\t\t\t\t\t\t|")
-        print("\t ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
+            print(f"     |\t    Balance:\t\t{self.currency}{self.balance:.2f}\t     |")
+        print("     |\t\t\t\t\t\t\t     |")
+        print("      ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
 
-    def perform_actions(self):
-        print(ascii.account)
-        print("[d] Return to Dashboard\t\t[l] Log Out\t[x] Exit Program\n")
+    def show_detailed(self):
+        print(ascii.account_details)
+        print("[g] Go to Dashboard\t[l] Log Out\t[x] Exit Program\n")
 
         account_menu_choice = input()
 
         # Check which choice the user made and proceed accordingly
         if (
-            account_menu_choice.lower() == "r"
+            account_menu_choice.lower() == "g"
             or account_menu_choice.lower() == "l"
             or account_menu_choice.lower() == "x"
         ):
-            if account_menu_choice.lower() == "r":
+            if account_menu_choice.lower() == "g":
                 # If the user would like to open a new bank account, instantiate the Bank_Account class with user passed in
                 return
             elif account_menu_choice.lower() == "l":
--- assignment/menu.py
+++ assignment/menu.py
@@ -1,3 +1,5 @@
+import sys
+
 from ascii import Ascii
 from bank_account import Bank_Account
 from user import User, log_in
@@ -10,60 +12,80 @@ ascii = Ascii()
 def display_authentication(users):
     print(ascii.cubs)
 
-    # Currently displays choice, regardless of any users existing or not
-    auth_choice = int(
-        input(
-            f"Welcome to CUBS Banking Limited's online services!\n{ascii.auth}\nPlease log in or register to continue.\n\n[1] Log In\n[2] Register\n\n"
-        )
-    )
-
-    # If the user chooses to log in, start the flow
-    if auth_choice == 1:
-        print(f"{ascii.log_in}\nEnter your email and password to log in.")
-
-        # Input user credentials
-        email = input("\nPlease enter your email address:\t")
-        password = input("Please enter your password:\t\t")
-
-        # Set the user object to be the returned user from log in function
-        user = log_in(email, password, users)
-
-        # Display the main dashboard to the user, passing in the logged in user and the users list
-        display_dashboard(user, users)
-
-    # If the user chooses to register a new account, start the flow
-    elif auth_choice == 2:
-        print(
-            f"{ascii.register}\nPlease provide your name, email and a unique password to create an account."
-        )
-
-        # Create a new user
-        new_user = User()
-
-        # Add the new user to the list of app's users
-        users.append(new_user)
-
-        # Prompt the user for beginning their journey by opening a bank account
-        open_bank_account_choice = int(
+    while True:
+        # Currently displays choice, regardless of any users existing or not
+        auth_choice = int(
             input(
-                "Would you like to get started by opening a bank account with CUBS Banking Limited?\n\n[1] Yes\n[2] No\n\n"
+                f"Welcome to CUBS Banking Limited's online services!\n{ascii.auth}\nPlease log in or register to continue.\n\n[1] Log In\n[2] Register\n\n"
             )
         )
 
-        # If the user would like to open a new bank account, start the flow
-        if open_bank_account_choice == 1:
-            # Create a new bank account for this new user
-            Bank_Account(new_user)
+        try:
+            # If the user chooses to log in, start the flow
+            if auth_choice == 1:
+                if not users:
+                    print(
+                        "\nUh oh! We don't actually have any users to log you in yet!\nPlease select option 2 in order to register an account with us.\n"
+                    )
+                    continue
 
-            # Send the user to the dashboard when done creating a bank account
-            display_dashboard(new_user, users)
+                else:
+                    print(f"{ascii.log_in}\nEnter your email and password to log in.")
 
-        # If the user does not want to open an account yet, send them straight to the dashboard
-        elif open_bank_account_choice == 2:
-            display_dashboard(new_user, users)
+                    # Input user credentials
+                    email = input("\nPlease enter your email address:\t")
+                    password = input("Please enter your password:\t\t")
 
-    # except:
-    #     print("Invalid choice. Please try again.")
+                    # Set the user object to be the returned user from log in function
+                    user = log_in(email, password, users)
+
+                    # Display the main dashboard to the user, passing in the logged in user and the users list
+                    display_dashboard(user, users)
+
+                    break
+
+            # If the user chooses to register a new account, start the flow
+            elif auth_choice == 2:
+                print(
+                    f"{ascii.register}\nPlease provide your name, email and a unique password to create an account."
+                )
+
+                # Create a new user
+                new_user = User()
+
+                # Add the new user to the list of app's users
+                users.append(new_user)
+
+                # Prompt the user for beginning their journey by opening a bank account
+                open_bank_account_choice = int(
+                    input(
+                        "Would you like to get started by opening a bank account with CUBS Banking Limited?\n\n[1] Yes\n[2] No\n\n"
+                    )
+                )
+
+                # If the user would like to open a new bank account, start the flow
+                if open_bank_account_choice == 1:
+                    # Create a new bank account for this new user
+                    Bank_Account(new_user)
+
+                    # Send the user to the dashboard when done creating a bank account
+                    display_dashboard(new_user, users)
+
+                    break
+
+                # If the user does not want to open an account yet, send them straight to the dashboard
+                elif open_bank_account_choice == 2:
+                    display_dashboard(new_user, users)
+
+                    break
+
+            # else:
+            #     break
+
+        except:
+            pass
+
+        print("Invalid choice. Please try again.")
 
 
 # Display the main
@@ -79,36 +101,43 @@ def display_dashboard(user, users):
         print(ascii.my_accounts)
         print("Enter account's corner number to perform account-specific operations.")
         user.show_bank_accounts()
-        main_menu_choice = input()
 
-        # Use an input() for the user to choose an action they'd like to perform
-
-        # Check which choice the user made and proceed accordingly
-        if (
-            main_menu_choice.lower() == "n"
-            or main_menu_choice.lower() == "l"
-            or main_menu_choice.lower() == "x"
-        ):
-            if main_menu_choice.lower() == "n":
-                # If the user would like to open a new bank account, instantiate the Bank_Account class with user passed in
-                Bank_Account(user)
-            elif main_menu_choice.lower() == "l":
-                # If the user wants to log out but not end the program, call the user.log_out() method
-                print(f"{ascii.see_you_soon}")
-                user.log_out()
-                # display_authentication(users)
-            elif main_menu_choice.lower() == "x":
-                # If the user would like to exit the program, call the exit() function
-                print(f"\n{ascii.goodbye}\n")
-                exit()
+        # while True:
+        try:
+            # Use an input() for the user to choose an action they'd like to perform
+            main_menu_choice = input()
+            # Check which choice the user made and proceed accordingly
+            if (
+                main_menu_choice.lower() == "n"
+                or main_menu_choice.lower() == "l"
+                or main_menu_choice.lower() == "x"
+            ):
+                if main_menu_choice.lower() == "n":
+                    # If the user would like to open a new bank account, instantiate the Bank_Account class with user passed in
+                    Bank_Account(user)
+                    break
+                elif main_menu_choice.lower() == "l":
+                    # If the user wants to log out but not end the program, call the user.log_out() method
+                    print(f"{ascii.see_you_soon}")
+                    user.log_out()
+                    break
+                elif main_menu_choice.lower() == "x":
+                    # If the user would like to exit the program, call the exit() function
+                    print(f"\n{ascii.goodbye}\n")
+                    raise SystemExit
             else:
-                return
-        else:
-            main_menu_choice = int(main_menu_choice)
-            user.bank_accounts[main_menu_choice - 1].perform_actions()
+                main_menu_choice = int(main_menu_choice)
+                user.bank_accounts[main_menu_choice - 1].show_detailed()
+                break
+        except SystemExit:
+            break
+        except:
+            pass
 
-    else:
-        print(
-            "\n======================================================================"
-        )
-        display_authentication(users)
+        print("\nInvalid choice. Please try again.\n")
+
+    # else:
+    #     print(
+    #         "\n======================================================================"
+    #     )
+    #     display_authentication(users)
